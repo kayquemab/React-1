6970232e64e07a520d69425cd07c97584f8ebd42
Kayquemab <kayquemiqueias17@gmail.com>
2025-07-25 13:50:12 -0300

    React, Material UI 5 e Typescript: #03

--- Clone-youtube/src/App.tsx
+++ Clone-youtube/src/App.tsx
@@ -1,11 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div className="App">
-      <>
-      Projeto React + TypeScript + Material UI
-      </>
-    </div>
+    <BrowserRouter>
+      <AppRoutes  />
+    </BrowserRouter>
+
   );
 }
 
