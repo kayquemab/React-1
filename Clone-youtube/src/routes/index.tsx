88afcf8fde8131f5f0726d3fdd39eaff40a7f269
Kayque Miqueias <kayquemiqueias17@gmail.com>
2025-07-28 12:41:52 -0300

    React, Material UI 5 e Typescript: #04

--- Clone-youtube/src/routes/index.tsx
+++ Clone-youtube/src/routes/index.tsx
@@ -1,11 +1,18 @@
+import { Button } from "@mui/material"
 import { Navigate, Route, Routes } from "react-router-dom"
 
 export const AppRoutes = () => {
-    return(
-    <Routes>
-        <Route path="/pagina-inicial" element={<p>Página Incial</p>}/>
+    return (
 
-        <Route path="*" element={<Navigate to="/pagina-inicial" />} />
-    </Routes>
+        <>
+            <Routes>
+                <Route path="/pagina-inicial" element={<p>Página Incial</p>} />
+
+                <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+            </Routes>
+
+            <Button>Teste</Button>
+        </>
+
     )
 }
